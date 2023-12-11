19bd1980f02d2ebec5140f9e11e421e6761487d2
Artem <avdmitriev_2@edu.hse.ru>
2023-12-12 02:17:12 +0300

    [HW1] Done.

--- src/main/java/ru/mts/Main.java
+++ src/main/java/ru/mts/Main.java
@@ -1,7 +1,24 @@
 package ru.mts;
 
+import ru.mts.models.Storage;
+
+import java.text.DecimalFormat;
+
 public class Main {
+
+    public static void getStoragePrice(Storage storage) {
+        double price = storage.getAmount() * storage.getPrice();
+        double priceWithDiscount = price * (1 - storage.getDiscount() / 100);
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(storage);
+        System.out.println("Итоговая сумма без учета скидки: " + df.format(price));
+        System.out.println("Итоговая сумма с учетом скидки: " + df.format(priceWithDiscount) + "\n");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        getStoragePrice(new Storage(10, 100, 0.75));
+        getStoragePrice(new Storage(5, 43, 42.575));
+        getStoragePrice(new Storage(1000, 754, 59.1));
     }
 }
---
+++ src/main/java/ru/mts/models/Storage.java
@@ -0,0 +1,39 @@
+package ru.mts.models;
+
+/*
+    Класс, имитирующий работу склада.
+    С помощью него можно рассчитать стоимость всех товаров с учетом скидки.
+ */
+public class Storage {
+
+    // Количество товара. Целочисленное значение.
+    private final int amount;
+
+    // Цена одной единицы товара. Вещественное значение.
+    private final double price;
+
+    // Размер скидки на товар. Вещественное значение.
+    private final double discount;
+
+    public Storage(int amount, double price, double discount) {
+        this.amount = amount;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public String toString() {
+        return "Storage: [amount = " + amount + "; price = " + price + "; discount = " + discount + "]";
+    }
+}
