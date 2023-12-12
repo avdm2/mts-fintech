c63b15b64520b15b812e9b4dc41b56d35021c9e7
Artem <avdmitriev_2@edu.hse.ru>
2023-12-12 17:04:49 +0300

    [HW1] Changed rounding and printing logic.

--- src/main/java/ru/mts/Main.java
+++ src/main/java/ru/mts/Main.java
@@ -2,23 +2,28 @@ package ru.mts;
 
 import ru.mts.models.Storage;
 
-import java.text.DecimalFormat;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Main {
 
-    public static void getStoragePrice(Storage storage) {
+    public static void calculateStoragePrice(Storage storage) {
         double price = storage.getAmount() * storage.getPrice();
         double priceWithDiscount = price * (1 - storage.getDiscount() / 100);
 
-        DecimalFormat df = new DecimalFormat("#.##");
+        BigDecimal decimalPrice = new BigDecimal(Double.toString(price))
+                .setScale(2, RoundingMode.HALF_UP);
+        BigDecimal decimalPriceWithDiscount = new BigDecimal(Double.toString(priceWithDiscount))
+                .setScale(2, RoundingMode.HALF_UP);
+
         System.out.println(storage);
-        System.out.println("Итоговая сумма без учета скидки: " + df.format(price));
-        System.out.println("Итоговая сумма с учетом скидки: " + df.format(priceWithDiscount) + "\n");
+        System.out.printf("Итоговая сумма без учета скидки: %.2f\n", decimalPrice.doubleValue());
+        System.out.printf("Итоговая сумма с учетом скидки: %.2f\n\n", decimalPriceWithDiscount.doubleValue());
     }
 
     public static void main(String[] args) {
-        getStoragePrice(new Storage(10, 100, 0.75));
-        getStoragePrice(new Storage(5, 43, 42.575));
-        getStoragePrice(new Storage(1000, 754, 59.1));
+        calculateStoragePrice(new Storage(10, 100, 0.75));
+        calculateStoragePrice(new Storage(5, 43, 42.575));
+        calculateStoragePrice(new Storage(1000, 754, 59.1));
     }
 }
